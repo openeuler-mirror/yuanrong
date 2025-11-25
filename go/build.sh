72ce17b9b22895fceeb6490cb82f58de586ec110
wxq <wangxiqian4@h-partners.com>
2025-11-25 09:55:41 +0800

    bugfix

--- go/build.sh
+++ go/build.sh
@@ -97,12 +97,12 @@ CC='gcc -fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2' go build -tags="${BUIL
 "${OUTPUT_DIR}"/bin/collector "${PROJECT_DIR}"/cmd/collector/main.go
 
 cd "${OUTPUT_DIR}"
-TAR_NAME="yr-dashboard-${VERSION}.tar.gz"
+DASHBOARD_TAR_NAME="yr-dashboard-${VERSION}.tar.gz"
 if [ -z "${VERSION}" ]; then
-    TAR_NAME="yr-dashboard.tar.gz"
+    DASHBOARD_TAR_NAME="yr-dashboard.tar.gz"
 fi
-tar -czvf "${TAR_NAME}" ./*
+tar -czvf "${DASHBOARD_TAR_NAME}" ./*
 mkdir -p "${RUNTIME_OUTPUT_DIR}"
-rm -rf "${RUNTIME_OUTPUT_DIR}/${TAR_NAME}"
-cp "${TAR_NAME}" "${RUNTIME_OUTPUT_DIR}"
+rm -rf "${RUNTIME_OUTPUT_DIR}/${DASHBOARD_TAR_NAME}"
+cp "${DASHBOARD_TAR_NAME}" "${RUNTIME_OUTPUT_DIR}"
 cd "${PROJECT_DIR}"

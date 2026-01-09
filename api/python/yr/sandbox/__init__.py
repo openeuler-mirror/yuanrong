2f4570b80b2068d4632dd38061715143de41b0e3
ycwang <wangyuchao12@huawei.com>
2026-01-09 12:05:18 +0800

    feat: add sandbox api
    Signed-off-by: ycwang <wangyuchao12@huawei.com>

---
+++ api/python/yr/sandbox/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Sandbox module for isolated code execution."""
+
+from yr.sandbox.sandbox import SandBox, SandBoxInstance, create
+
+__all__ = ['SandBox', 'SandBoxInstance', 'create']
